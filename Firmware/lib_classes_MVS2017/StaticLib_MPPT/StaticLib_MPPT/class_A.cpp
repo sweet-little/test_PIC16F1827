c8cc785c82298037a1bd1ae48b3833f9889cb860
sweet-little <42453852+sweet-little@users.noreply.github.com>
2019-07-07 21:19:17 +0300

    first chahnging in class_A

--- Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.cpp
+++ Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.cpp
@@ -1,12 +1,7 @@
 #include "stdafx.h"
 #include "class_A.h"
 
+static constexpr int temp[] = { 1, 2, 3, 4 };
+static class_A za(temp);
 
-class_A::class_A()
-{
-}
-
-
-class_A::~class_A()
-{
-}
+static int iz = za(temp,1);
--- Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.h
+++ Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.h
@@ -1,8 +1,22 @@
 #pragma once
-class class_A
+
+class iclass_A
 {
 public:
-	class_A();
-	~class_A();
+	virtual ~iclass_A() {}
+	virtual int operator()(const int*, const int) = 0;
+};
+
+constexpr unsigned N = 20;
+
+template<typename T>
+class class_A
+	: public iclass_A
+{
+	T* data;
+public:
+	class_A(T* data) : data(data) { }
+	~class_A() {}
+	int operator()(const int* buffer, const int size) override { return 0; }
 };
 
