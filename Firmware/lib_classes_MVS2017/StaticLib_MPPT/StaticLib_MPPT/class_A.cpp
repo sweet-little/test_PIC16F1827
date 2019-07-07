3077cb977a6660dd78ee8b4b8ca411bbaf70da69
sweet-little <42453852+sweet-little@users.noreply.github.com>
2019-07-07 19:57:08 +0300

    add class_A
    add class_A in StaticLib_MPPT

---
+++ Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "class_A.h"
+
+
+class_A::class_A()
+{
+}
+
+
+class_A::~class_A()
+{
+}
---
+++ Firmware/lib_classes_MVS2017/StaticLib_MPPT/StaticLib_MPPT/class_A.h
@@ -0,0 +1,8 @@
+#pragma once
+class class_A
+{
+public:
+	class_A();
+	~class_A();
+};
+
